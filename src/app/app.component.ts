770b5e9d2e49e9a58ab40c88c0ed72240121be1b
Ernesto Pestaña <epestanamx@gmail.com>
2017-12-14 18:00:53 -0500

    Basicos de navegación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { Ajustes2Page } from '../pages/ajustes2/ajustes2';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  tabs: any = TabsPage;
+  ajustes2: any = Ajustes2Page;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage:any = TabsPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  abrirPagina(pagina: any) {
+    this.rootPage = pagina;
+    this.menuCtrl.close();
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,45 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { AjustesPage } from "../pages/ajustes/ajustes";
+import { Ajustes2Page } from "../pages/ajustes2/ajustes2";
+import { ModalPage } from "../pages/modal/modal";
+import { Pagina1Page } from "../pages/pagina1/pagina1";
+import { Pagina2Page } from "../pages/pagina2/pagina2";
+import { Pagina3Page } from "../pages/pagina3/pagina3";
+import { PrincipalPage } from "../pages/principal/principal";
+import { TabsPage } from "../pages/tabs/tabs";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina1Page,
+    Pagina2Page,
+    Pagina3Page,
+    PrincipalPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      backButtonText:'Atras'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina1Page,
+    Pagina2Page,
+    Pagina3Page,
+    PrincipalPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
